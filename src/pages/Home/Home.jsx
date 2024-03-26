b93ba5ded0567223037690e6d80a7bb20122fd8f
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-03-27 01:32:39 +0600

    sending new message option is created

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,13 @@
 import { useContext, useEffect, useState } from "react";
-import { AuthContext } from "../../ContextApi/AuthProvider";
 import axios from "axios";
+import { AuthContext } from "../../ContextApi/AuthProvider";
 const Home = () => {
     const { user } = useContext(AuthContext)
     const [conversations, setConversations] = useState([])
     const [messages, setMessages] = useState([])
+    const [message, setMessage] = useState(null)
     const userDetails = JSON.parse(localStorage.getItem('userDetails'));
     const id = userDetails?.user.id
-    console.log(id)
     useEffect(() => {
         axios.get(`http://localhost:5000/api/conversation/${id}`)
             .then(res => {
@@ -47,16 +47,22 @@ const Home = () => {
     //     },
     // ]
 
-    const fetchMessages = async (conversationId) => {
+    const fetchMessages = async (conversationId, user) => {
         axios.get(`http://localhost:5000/api/message/${conversationId}`)
             .then(res => {
-                setMessages(res.data)
+                setMessages({ messages: res.data, receiver: user, conversationId })
             })
             .catch(err => {
                 console.error(err)
             })
     }
-    console.log(messages)
+    const sendMessage = () => {
+        axios.post('http://localhost:5000/api/message', { conversationId: messages?.conversationId, senderId: id, message: message, receiverId: messages?.receiver?.receiverId})
+            .then(res => {
+                setMessage(res.data)
+                setMessage('')
+            })
+    }
     return (
         <div className="w-screen flex bg-[#d4f4fc]">
             <div className="w-[25%] h-screen bg-[#f3f5ff]">
@@ -81,7 +87,7 @@ const Home = () => {
                                     console.log(conversationId)
                                     return (
                                         <div key={user?.id} className="flex items-center py-4 border-b border-gray-300 mr-10">
-                                            <div onClick={() => fetchMessages(conversationId)} className="cursor-pointer flex items-center">
+                                            <div onClick={() => fetchMessages(conversationId, user)} className="cursor-pointer flex items-center">
                                                 <div>
                                                     <img src={'img1'} alt="" width={50} height={50} className="rounded-full" />
                                                 </div>
@@ -98,37 +104,39 @@ const Home = () => {
                 </div>
             </div>
             <div className="w-[50%] h-screen bg-white flex flex-col items-center">
-                <div className="w-[75%] bg-[#f3f5ff] h-[80px] my-8 rounded-full flex items-center px-14">
-                    <div className="cursor-pointer"><img src={user?.photoURL} width={60} height={60} alt="" className="rounded-full" /></div>
-                    <div className=" ml-4 mr-auto">
-                        <h3 className="text-lg font-semibold">{user?.displayName}</h3>
-                        <p className="text-sm font-light text-gray-500">active</p>
+                {
+                    messages?.receiver?.fullName &&
+                    <div className="w-[75%] bg-[#f3f5ff] h-[80px] my-8 rounded-full flex items-center px-14 py-6">
+                        <div className="cursor-pointer"><img src={''} width={60} height={60} alt="" className="rounded-full" /></div>
+                        <div className=" ml-4 mr-auto">
+                            <h3 className="text-lg font-semibold">{messages?.receiver?.fullName}</h3>
+                            <h3 className="text-sm font-extralight py-1">{messages?.receiver?.email}</h3>
+                        </div>
+                        <div className="cursor-pointer flex gap-5">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-phone-outgoing" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="black" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
+                                <path d="M15 9l5 -5" />
+                                <path d="M16 4l4 0l0 4" />
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-video" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="black" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M15 10l4.553 -2.276a1 1 0 0 1 1.447 .894v6.764a1 1 0 0 1 -1.447 .894l-4.553 -2.276v-4z" />
+                                <path d="M3 6m0 2a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v8a2 2 0 0 1 -2 2h-8a2 2 0 0 1 -2 -2z" />
+                            </svg>
+                        </div>
                     </div>
-                    <div className="cursor-pointer flex gap-5">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-phone-outgoing" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="black" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
-                            <path d="M15 9l5 -5" />
-                            <path d="M16 4l4 0l0 4" />
-                        </svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-video" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="black" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path d="M15 10l4.553 -2.276a1 1 0 0 1 1.447 .894v6.764a1 1 0 0 1 -1.447 .894l-4.553 -2.276v-4z" />
-                            <path d="M3 6m0 2a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v8a2 2 0 0 1 -2 2h-8a2 2 0 0 1 -2 -2z" />
-                        </svg>
-                    </div>
-                </div>
+                }
                 <div className="h-[75%] w-full overflow-scroll overflow-x-hidden shadow-sm">
                     <div className=" p-14">
                         {
-                            messages.length > 0 ?
-                                messages?.map((messageObj, i) => {
-                                    const {message, user} = messageObj;
+                            messages?.messages?.length > 0 ?
+                                messages?.messages?.map((messageObj, i) => {
+                                    const { message, user } = messageObj;
                                     return (
                                         <div
-                                            className={`p-4 max-w-[50%] mb-6 rounded-b-xl rounded-tr-xl ${
-                                                id === user?.id ? "bg-[#1476ff] text-white ml-auto" : "bg-[#f3f5ff]"
-                                            }`}
+                                            className={`p-4 max-w-[50%] mb-6 rounded-b-xl rounded-tr-xl ${id === user?.id ? "bg-[#1476ff] text-white ml-auto" : "bg-[#f3f5ff]"
+                                                }`}
                                             key={i}
                                         >
                                             {message}
@@ -137,35 +145,37 @@ const Home = () => {
 
                                 })
                                 :
-                                <div className="text-center text-lg font-semibold mt-24">No Messages</div>
+                                <div className="text-center text-lg font-semibold mt-24">No Messages Please Start A Conversation</div>
                         }
 
                     </div>
                 </div>
-                <div className="w-full p-10 flex items-center">
-                    <input type="text" placeholder="type a message......" className="w-full p-3 shadow-md bg-[#f9faff] focus:ring-0 focus:border-0 outline-none rounded-full border-0" />
-                    <div className="ml-4 bg-[#f9faff] cursor-pointer rounded-full p-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-send" width="30" height="30" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#2c3e50" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path d="M10 14l11 -11" />
-                            <path d="M21 3l-6.5 18a.55 .55 0 0 1 -1 0l-3.5 -7l-7 -3.5a.55 .55 0 0 1 0 -1l18 -6.5" />
-                        </svg>
-                    </div>
-                    <div className="ml-1 bg-[#f9faff] cursor-pointer rounded-full p-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-circle-plus" width="30" height="30" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#2c3e50" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
-                            <path d="M9 12h6" />
-                            <path d="M12 9v6" />
-                            {/* <div className="p-4 max-w-[50%] mb-6 bg-[#1476ff] rounded-b-xl rounded-tr-xl ml-auto text-white" key={i}>
+                {
+                    messages?.receiver?.fullName && <div className="w-full p-10 flex items-center">
+                        <input type="text" value={message} onChange={(e) => setMessage(e.target.value)} placeholder="type a message......" className="w-full p-3 shadow-md bg-[#f9faff] focus:ring-0 focus:border-0 outline-none rounded-full border-0" />
+                        <div className={`ml-4 bg-[#f9faff] cursor-pointer rounded-full p-2 ${!message && 'pointer-events-none'}`} onClick={() => sendMessage()}>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-send" width="30" height="30" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#2c3e50" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M10 14l11 -11" />
+                                <path d="M21 3l-6.5 18a.55 .55 0 0 1 -1 0l-3.5 -7l-7 -3.5a.55 .55 0 0 1 0 -1l18 -6.5" />
+                            </svg>
+                        </div>
+                        <div className={`ml-1 bg-[#f9faff] cursor-pointer rounded-full p-2 ${!message && 'pointer-events-none'}`}>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-circle-plus" width="30" height="30" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#2c3e50" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
+                                <path d="M9 12h6" />
+                                <path d="M12 9v6" />
+                                {/* <div className="p-4 max-w-[50%] mb-6 bg-[#1476ff] rounded-b-xl rounded-tr-xl ml-auto text-white" key={i}>
                                                 {message}
                                             </div> */}
-                        </svg>
-                    </div>
-                    {/* <div className="p-4 max-w-[50%] mb-6 bg-[#f3f5ff] rounded-b-xl rounded-tr-xl" key={user?.id}>
+                            </svg>
+                        </div>
+                        {/* <div className="p-4 max-w-[50%] mb-6 bg-[#f3f5ff] rounded-b-xl rounded-tr-xl" key={user?.id}>
                                                 {message}
                                             </div> */}
-                </div>
+                    </div>
+                }
             </div>
             <div className="w-[25%] h-screen bg-[#f9faff]"></div>
         </div>
